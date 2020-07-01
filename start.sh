fe4e3acb38288776e54e21f3e1e1201a2256f961
Rickard Östergård <rickard.ostergard@gmail.com>
2020-07-01 13:53:02 +0200

    change proxy to host

--- start.sh
+++ start.sh
@@ -8,51 +8,53 @@ close_connection() {
 trap close_connection TERM
 
 case "$1" in
-  "proxy" )
-    TUNNELS=""
+"proxy")
+  TUNNELS=""
 
-    for MAPPINGS in `echo ${PORTS} | awk -F, '{for (i=1;i<=NF;i++)print $i}'`; do
-      IFS=':' read -r -a MAPPING <<< "$MAPPINGS"; unset IFS
+  for MAPPINGS in $(echo ${PORTS} | awk -F, '{for (i=1;i<=NF;i++)print $i}'); do
+    IFS=':' read -r -a MAPPING <<<"$MAPPINGS"
+    unset IFS
 
-      read -r -d '' TUNNELS <<-EOS
+    read -r -d '' TUNNELS <<-EOS
 ${TUNNELS}
 
 server {
     listen ${MAPPING[0]};
 
-    proxy_pass 127.0.0.1:${MAPPING[1]};
+    proxy_pass host.docker.internal:${MAPPING[1]};
     proxy_responses 0;
 }
 EOS
-    done
+  done
 
-    export TUNNELS
+  export TUNNELS
 
-    bash -c "envsubst < /nginx.conf.template > /etc/nginx/nginx.conf && nginx -g 'daemon off;'"
-    ;;
+  bash -c "envsubst < /nginx.conf.template > /etc/nginx/nginx.conf && nginx -g 'daemon off;'"
+  ;;
 
-  "app" )
-    DOCKER_HOST="$(getent hosts host.docker.internal | cut -d' ' -f1)"
-    APP_IP="${APP_IP:-DOCKER_HOST}"
+"app")
+  DOCKER_HOST="$(getent hosts host.docker.internal | cut -d' ' -f1)"
+  APP_IP="${APP_IP:-DOCKER_HOST}"
 
-    if [ -z "${APP_IP}" ]; then
-      APP_IP=$(ip -4 route show default | cut -d' ' -f3)
-    fi
+  if [ -z "${APP_IP}" ]; then
+    APP_IP=$(ip -4 route show default | cut -d' ' -f3)
+  fi
 
-    TUNNELS=" "
+  TUNNELS=" "
 
-    for MAPPINGS in `echo ${PORTS} | awk -F, '{for (i=1;i<=NF;i++)print $i}'`; do
-      IFS=':' read -r -a MAPPING <<< "$MAPPINGS"; unset IFS
-      TUNNELS="${TUNNELS} -R ${MAPPING[1]}:${APP_IP}:${MAPPING[0]} "
-    done
+  for MAPPINGS in $(echo ${PORTS} | awk -F, '{for (i=1;i<=NF;i++)print $i}'); do
+    IFS=':' read -r -a MAPPING <<<"$MAPPINGS"
+    unset IFS
+    TUNNELS="${TUNNELS} -R ${MAPPING[1]}:${APP_IP}:${MAPPING[0]} "
+  done
 
-    autossh -M 0 -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "StrictHostKeyChecking=no" -o "ServerAliveInterval=5" -o "ServerAliveCountMax 3" -i /ssh.key ${TUNNELS} ${PROXY_SSH_USER}@${PROXY_HOST} -p ${PROXY_SSH_PORT}
+  autossh -M 0 -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "StrictHostKeyChecking=no" -o "ServerAliveInterval=5" -o "ServerAliveCountMax 3" -i /ssh.key ${TUNNELS} ${PROXY_SSH_USER}@${PROXY_HOST} -p ${PROXY_SSH_PORT}
 
-    while true; do
-      sleep 1 &
-      wait $!
-    done
+  while true; do
+    sleep 1 &
+    wait $!
+  done
 
-    exit 0
-    ;;
+  exit 0
+  ;;
 esac
